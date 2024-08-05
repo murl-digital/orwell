892a1088856e915595ce77c2811d28c46f9bd9ad
joe <hello@murl.digital>
2024-08-05 14:51:15 -0600

    stream email events

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 use std::{
     io::{BufWriter, Cursor},
     sync::LazyLock,
+    time::Duration,
 };
 
 use axum::{
     extract::{MatchedPath, Query, Request},
     http::{header, Response, StatusCode},
-    response::IntoResponse,
+    response::{
+        self,
+        sse::{Event, KeepAlive},
+        IntoResponse, Sse,
+    },
     routing::{get, post},
     Json, Router,
 };
+use futures_util::{Stream, StreamExt, TryStreamExt};
 use image::{Rgba, RgbaImage};
 use serde::{Deserialize, Serialize};
 use surrealdb::{
     engine::local::{Db, SpeeDb},
-    Surreal,
+    opt::RecordId,
+    sql::{Id, Object},
+    Notification, Surreal,
 };
+use thiserror::Error;
 use tokio::net::TcpListener;
+use tracing::error;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 static PIXEL: LazyLock<Vec<u8>> = LazyLock::new(|| {
@@ -29,12 +39,43 @@ static PIXEL: LazyLock<Vec<u8>> = LazyLock::new(|| {
 });
 
 #[derive(Serialize, Deserialize)]
-struct Tag {
+struct RawTag {
+    id: RecordId,
     to: String,
     subject: String,
-    read: bool,
+    read: i32,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Tag {
+    id: String,
+    to: String,
+    subject: String,
+    read: i32,
+}
+
+#[derive(Debug)]
+struct IncompatibleId;
+
+impl TryFrom<RawTag> for Tag {
+    type Error = IncompatibleId;
+
+    fn try_from(value: RawTag) -> Result<Self, Self::Error> {
+        if let Id::String(id) = value.id.id {
+            Ok(Self {
+                id,
+                to: value.to,
+                subject: value.subject,
+                read: value.read,
+            })
+        } else {
+            Err(IncompatibleId)
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+#[error(transparent)]
 struct DatabaseError(surrealdb::Error);
 
 impl IntoResponse for DatabaseError {
@@ -64,15 +105,15 @@ async fn get_image(
 
     if let Some(mut tag) = state
         .db
-        .select::<Option<Tag>>(("tags", id.as_str()))
+        .select::<Option<RawTag>>(("tags", id.as_str()))
         .await
         .map_err(DatabaseError)?
     {
-        tag.read = true;
+        tag.read += 1;
 
         state
             .db
-            .update::<Option<Tag>>(("tags", id.as_str()))
+            .update::<Option<RawTag>>(("tags", id.as_str()))
             .merge(tag)
             .await
             .map_err(DatabaseError)?;
@@ -100,7 +141,7 @@ async fn create_tag(
 
     let result: String = state
         .db
-        .query("CREATE tags SET to = $to, subject = $subject, read = false RETURN meta::id(id)")
+        .query("CREATE tags SET to = $to, subject = $subject, read = 0 RETURN meta::id(id)")
         .bind(query.0)
         .await
         .map_err(DatabaseError)?
@@ -109,6 +150,100 @@ async fn create_tag(
         .expect("there should defeinitely be a result at this point");
 
     Ok(result)
+}
+
+#[derive(Serialize)]
+#[serde(tag = "action", content = "d")]
+enum StreamAction {
+    Initialize(Vec<Tag>),
+    Create(Tag),
+    Update(Tag),
+    Delete(Tag),
+}
+
+impl TryFrom<Notification<RawTag>> for StreamAction {
+    type Error = IncompatibleId;
+
+    fn try_from(value: Notification<RawTag>) -> Result<Self, Self::Error> {
+        Ok(match value.action {
+            surrealdb::Action::Create => Self::Create(value.data.try_into()?),
+            surrealdb::Action::Update => Self::Update(value.data.try_into()?),
+            surrealdb::Action::Delete => Self::Delete(value.data.try_into()?),
+            _ => todo!(),
+        })
+    }
+}
+
+async fn get_tags(
+    state: axum::extract::State<State>,
+) -> Result<Sse<impl Stream<Item = Result<Event, DatabaseError>>>, DatabaseError> {
+    state
+        .db
+        .use_ns("production")
+        .use_db("orwell")
+        .await
+        .map_err(DatabaseError)?;
+
+    let db = state.db.to_owned();
+
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Result<Event, DatabaseError>>(128);
+
+    tokio::spawn(async move {
+        let query = db.select::<Vec<RawTag>>("tags");
+        match query.await {
+            Ok(data) => {
+                if tx
+                    .send(Ok(Event::default()
+                        .json_data(StreamAction::Initialize(
+                            data.into_iter()
+                                .map(Tag::try_from)
+                                .collect::<Result<_, _>>()
+                                .expect("tag ids should always be compatable"),
+                        ))
+                        .expect("huh")))
+                    .await
+                    .is_err()
+                {
+                    return;
+                }
+            }
+            Err(err) => {
+                tx.send(Err(DatabaseError(err))).await;
+                return;
+            }
+        }
+        let mut stream = db.select::<Vec<RawTag>>("tags").live().await.expect("huh");
+        while let Some(notif) = stream.next().await {
+            match notif {
+                Ok(notif) => {
+                    if tx
+                        .send(Ok(Event::default()
+                            .json_data(
+                                StreamAction::try_from(notif)
+                                    .expect("tag ids should always be compatable"),
+                            )
+                            .expect("why didn't this work?")))
+                        .await
+                        .is_err()
+                    {
+                        return;
+                    }
+                }
+                Err(err) => {
+                    tx.send(Err(DatabaseError(err))).await;
+                    return;
+                }
+            }
+        }
+    });
+
+    let stream = async_stream::stream! {
+        while let Some(item) = rx.recv().await {
+            yield item;
+        }
+    };
+
+    Ok(Sse::new(stream).keep_alive(KeepAlive::new()))
 }
 
 #[derive(Clone)]
@@ -130,6 +265,7 @@ async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", get(get_image))
         .route("/tags", post(create_tag))
+        .route("/tags", get(get_tags))
         .layer(
             tower_http::trace::TraceLayer::new_for_http()
                 .make_span_with(|req: &Request| {
